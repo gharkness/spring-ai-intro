5aab1abc5e62f5d5ff0b01235ae7d6ea8ecdbc85
Garrett Harkness <gharkness@sourceallies.com>
2025-06-08 16:57:08 -0500

    use json schema mapping

--- src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
@@ -2,6 +2,7 @@ package guru.springframework.springaiintro.controllers;
 
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 import guru.springframework.springaiintro.services.OpenAIService;
 import lombok.AllArgsConstructor;
@@ -15,8 +16,13 @@ public class QuestionController {
 
     private final OpenAIService openAIService;
 
+    @PostMapping("/capitalWithInfo")
+    public Answer getCapitalWithInfo(@RequestBody GetCapitalRequest getCapitalRequest) {
+        return openAIService.getCapitalWithInfo(getCapitalRequest);
+    }
+
     @PostMapping("/capital")
-    public Answer getCapital(@RequestBody GetCapitalRequest getCapitalRequest) {
+    public GetCapitalResponse getCapital(@RequestBody GetCapitalRequest getCapitalRequest) {
         return this.openAIService.getCapital(getCapitalRequest);
     }
 
--- src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiintro/services/OpenAIServiceImpl.java
@@ -1,7 +1,11 @@
 package guru.springframework.springaiintro.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.GetCapitalRequest;
+import guru.springframework.springaiintro.model.GetCapitalResponse;
 import guru.springframework.springaiintro.model.Question;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +15,8 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.converter.BeanOutputConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -22,20 +28,40 @@ public class OpenAIServiceImpl implements OpenAIService {
 
     private final ChatModel chatModel;
 
+    @Value("classpath:templates/get-capital-prompt.st")
+    private Resource getCapitalPrompt;
+
+    @Value("classpath:templates/get-capital-with-info.st")
+    private Resource getCapitalWithInfoPrompt;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
     public OpenAIServiceImpl(ChatModel chatModel) {
         this.chatModel = chatModel;
     }
 
-    @Value("classpath:templates/get-capital-prompt.st")
-    private Resource getCapitalPrompt;
-
     @Override
-    public Answer getCapital(GetCapitalRequest getCapitalRequest) {
-        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
+    public Answer getCapitalWithInfo(GetCapitalRequest getCapitalRequest) {
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalWithInfoPrompt);
         Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry()));
         ChatResponse response = chatModel.call(prompt);
 
         return new Answer(response.getResult().getOutput().getContent());
+    }
+
+    @Override
+    public GetCapitalResponse getCapital(GetCapitalRequest getCapitalRequest) {
+        BeanOutputConverter<GetCapitalResponse> converter = new BeanOutputConverter<>(GetCapitalResponse.class);
+        String format = converter.getFormat();
+
+        PromptTemplate promptTemplate = new PromptTemplate(getCapitalPrompt);
+        Prompt prompt = promptTemplate.create(Map.of("stateOrCountry", getCapitalRequest.stateOrCountry(),
+                "format", format));
+
+        ChatResponse response = chatModel.call(prompt);
+
+        return converter.convert(response.getResult().getOutput().getContent());
     }
 
     @Override
